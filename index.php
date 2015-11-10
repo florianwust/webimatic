791e5ff261ea2bffb194edd65dcc3bfcfd3351b2
Quentin HAENSLER <qhaensler@me.com>
2015-11-10 13:56:22 +0100

    Include PHP

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'header.html'; ?>
+		<div id="main" role="main">
+			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo aperiam a sapiente impedit, corporis quisquam tempore nihil in blanditiis aliquid harum quod, iusto, molestiae. Provident, nesciunt? Accusamus nostrum aspernatur amet?</div>
+			<div>Aliquam ab pariatur natus nulla modi nemo minima exercitationem dicta alias tenetur necessitatibus, animi vero iste maiores inventore recusandae laborum odio eius, id. Enim voluptatem fugiat et, dolor provident vel.</div>
+			<div>Neque, asperiores, sequi. Deserunt reiciendis consequatur dolorem, voluptas vero pariatur maxime magnam sit mollitia quis! Deleniti perferendis animi modi neque praesentium quod ex aut, quisquam expedita adipisci maiores, consequuntur qui.</div>
+			<div>Doloribus tempore facere dolores, magni qui atque velit quis perspiciatis quisquam ducimus omnis! Rem, porro doloremque rerum animi quibusdam assumenda. Provident vitae aspernatur aliquam itaque, quos perspiciatis reprehenderit eveniet est!</div>
+		</div>
+<?php include 'footer.html'; ?>
