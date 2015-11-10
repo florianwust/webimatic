f00a86a0889de2c51eb4ca81d5eb6c2557009d79
Quentin HAENSLER <qhaensler@me.com>
2015-11-10 16:32:44 +0100

    Chill - petites modifications - amélioration de knacks OKLM

--- index.php
+++ index.php
@@ -1,20 +1,28 @@
 <?php include 'header.html'; ?>
+<script>
+$(document).ready(function() {
+	$("#Menu_Home").addClass('active');
+});
+</script>
 <div class="wrapper home">
-	<h2>Accueil</h2>
 	<div id="fil-ariane">
 		Vous êtes dans :
 		Accueil >
 	</div>
+	<h2>Accueil</h2>
 	<div id="main" role="main" class="grid-3">
 		<div class="skills">
+			<img src="./media/1.png" class="imgcenter">
 			<h3>Site Web</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem enim totam in magni laudantium rem ex porro unde consectetur, quasi excepturi eligendi iusto quas blanditiis, qui, iure sunt repellat, magnam?</p>
 		</div>
 		<div class="skills">
+			<img src="./media/2.png" class="imgcenter">
 			<h3>Accessibilité</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem enim totam in magni laudantium rem ex porro unde consectetur, quasi excepturi eligendi iusto quas blanditiis, qui, iure sunt repellat, magnam?</p>
 		</div>
 		<div class="skills">
+			<img src="./media/3.png" class="imgcenter">
 			<h3>Responsive</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem enim totam in magni laudantium rem ex porro unde consectetur, quasi excepturi eligendi iusto quas blanditiis, qui, iure sunt repellat, magnam?</p>
 		</div>
---
+++ services.php
@@ -0,0 +1,28 @@
+<?php include 'header.html'; ?>
+<script>
+$(document).ready(function() {
+	$("#Menu_Services").addClass('active');
+});
+</script>
+<div class="wrapper home">
+	<div id="fil-ariane">
+		Vous êtes dans :
+		Accueil >
+	</div>
+	<h2>Accueil</h2>
+	<div id="main" role="main" class="grid-3">
+		<div class="skills">
+			<h3>Site Web</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem enim totam in magni laudantium rem ex porro unde consectetur, quasi excepturi eligendi iusto quas blanditiis, qui, iure sunt repellat, magnam?</p>
+		</div>
+		<div class="skills">
+			<h3>Accessibilité</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem enim totam in magni laudantium rem ex porro unde consectetur, quasi excepturi eligendi iusto quas blanditiis, qui, iure sunt repellat, magnam?</p>
+		</div>
+		<div class="skills">
+			<h3>Responsive</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem enim totam in magni laudantium rem ex porro unde consectetur, quasi excepturi eligendi iusto quas blanditiis, qui, iure sunt repellat, magnam?</p>
+		</div>
+	</div>
+</div>
+<?php include 'footer.html'; ?>
