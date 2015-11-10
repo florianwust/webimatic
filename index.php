befdff0c9b2929946e31dfd078e124cd99ef0a0f
Quentin HAENSLER <qhaensler@me.com>
2015-11-10 16:06:56 +0100

    edit index.php

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
 <?php include 'header.html'; ?>
-		<div id="fil-ariane">
-		   Vous êtes dans :
-		   Accueil >
+<div class="wrapper home">
+	<h2>Accueil</h2>
+	<div id="fil-ariane">
+		Vous êtes dans :
+		Accueil >
+	</div>
+	<div id="main" role="main" class="grid-3">
+		<div class="skills">
+			<h3>Site Web</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem enim totam in magni laudantium rem ex porro unde consectetur, quasi excepturi eligendi iusto quas blanditiis, qui, iure sunt repellat, magnam?</p>
 		</div>
-		<div id="main" role="main">
-			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo aperiam a sapiente impedit, corporis quisquam tempore nihil in blanditiis aliquid harum quod, iusto, molestiae. Provident, nesciunt? Accusamus nostrum aspernatur amet?</div>
-			<div>Aliquam ab pariatur natus nulla modi nemo minima exercitationem dicta alias tenetur necessitatibus, animi vero iste maiores inventore recusandae laborum odio eius, id. Enim voluptatem fugiat et, dolor provident vel.</div>
-			<div>Neque, asperiores, sequi. Deserunt reiciendis consequatur dolorem, voluptas vero pariatur maxime magnam sit mollitia quis! Deleniti perferendis animi modi neque praesentium quod ex aut, quisquam expedita adipisci maiores, consequuntur qui.</div>
-			<div>Doloribus tempore facere dolores, magni qui atque velit quis perspiciatis quisquam ducimus omnis! Rem, porro doloremque rerum animi quibusdam assumenda. Provident vitae aspernatur aliquam itaque, quos perspiciatis reprehenderit eveniet est!</div>
+		<div class="skills">
+			<h3>Accessibilité</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem enim totam in magni laudantium rem ex porro unde consectetur, quasi excepturi eligendi iusto quas blanditiis, qui, iure sunt repellat, magnam?</p>
 		</div>
+		<div class="skills">
+			<h3>Responsive</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem enim totam in magni laudantium rem ex porro unde consectetur, quasi excepturi eligendi iusto quas blanditiis, qui, iure sunt repellat, magnam?</p>
+		</div>
+	</div>
+</div>
 <?php include 'footer.html'; ?>
