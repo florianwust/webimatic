b526fab95f83e42fc8018a6866b38b8ae3fd830b
Florian Wust <contact@florianwust.fr>
2015-11-10 14:21:28 +0100

    AJAX table et tout et tout

--- prix.php
+++ prix.php
@@ -1,23 +1,23 @@
 <?php include 'header.html'; ?>
-<table summary="Tableau modifiable présentant les oeuvres de plusieurs réalisateurs de films, plusieurs oeuvres par réaliasateur sont possibles, accompagnés de leur date de réalisation, titre et genre filmique." id="megaTab">
-  <caption>Liste non exhaustive de films</caption>
+<table summary="Tableau présentant les différents tarifs des prestations de l'agence Webimatic" id="megaTab">
+  <caption>Nos tarifs</caption>
   <tbody>
     <tr>
-      <th id="l1c1">Realisateur</th>
-      <th id="l1c2">Titre du film</th>
-      <th id="l1c3">Année de réalisation</th>
-      <th id="l1c4">Genre</th>
+      <th id="l1c1">Type de produit</th>
+      <th id="l1c2">Modèle</th>
+      <th id="l1c3">Prix HT</th>
+      <th id="l1c4">Prix TTC</th>
     </tr>
     <tr>
-      <th id="l2c1" headers="l1c1" rowspan="2">David Lynch</th>
-      <th id="l2c2" headers="l1c2 l2c1">Lost Highway</th>
-      <td tabindex="40" id=1 headers="l1c3 l2c1 l2c2">Champs php</td>
-      <td tabindex="50" id=2 headers="l1c4 l2c1 l2c2">Champs php</td>
+      <th id="l2c1" headers="l1c1" rowspan="2">Site vitrine</th>
+      <th id="l2c2" headers="l1c2 l2c1">One page</th>
+      <td tabindex="40" id=1 headers="l1c3 l2c1 l2c2">10€</td>
+      <td tabindex="50" id=2 headers="l1c4 l2c1 l2c2">10€</td>
     </tr>
     <tr>
-      <th id="l3c2" headers="l1c2 l2c1">Blue Velvet</th>
-      <td tabindex="60" id=3 headers="l1c3 l2c1 l3c2">Champs php</td>
-      <td tabindex="70" id=4 headers="l1c4 l2c1 l3c2">Champs php</td>
+      <th id="l3c2" headers="l1c2 l2c1">Regular</th>
+      <td tabindex="60" id=3 headers="l1c3 l2c1 l3c2">10€</td>
+      <td tabindex="70" id=4 headers="l1c4 l2c1 l3c2">10€</td>
     </tr>
   </tbody>
 </table>
